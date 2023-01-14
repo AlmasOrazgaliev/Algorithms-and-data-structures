eeaaf4da4072168cc59389d9b64c8da6d80b61ba
AlmasOrazgaliev <98646926+AlmasOrazgaliev@users.noreply.github.com>
2023-01-14 13:15:34 +0600

    Delete t.py

--- lab9/t.py
+++
@@ -1,9 +0,0 @@
-def is_person_teenager(n):
-    if n>=12 and n<=17:
-        return True
-    else: return False
-
-n = int(input())
-if is_person_teenager(n):
-    print('True')
-else: print('False')
